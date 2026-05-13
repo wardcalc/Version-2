708a3c4fac16e1f2b567016260b974dbd9232de0
wardcalc <wardcalc.med@gmail.com>
2026-05-13 16:33:50 +0100

    Update uz.js

--- js/locales/uz.js
+++ js/locales/uz.js
@@ -1,70 +1,148 @@
 const uz = {
-    "nav_dashboard": "Boshqaruv", "nav_favorites": "Sevimlilar", "nav_emergency": "SHOSHILINCH", "search_placeholder": "Qidirish (masalan, GCS, BMI)...",
-    "dash_title": "Klinik katalog", "dash_subtitle": "Toifani tanlang yoki qidiring.",
-    "hero_pill": "TIBBIYOT TALABASI TOMONIDAN YARATILGAN",
-    "hero_title_1": "Klinik asboblar", "hero_title_2": "palata aylanmasi uchun",
-    "hero_subtitle": "4 tilda dalillarga asoslangan kalkulyatorlar. Tez, ishonchli va mobil qurilmalarga mos.",
-    "stat_student": "Talaba tomonidan", "stat_tools": "Kalkulyatorlar", "stat_langs": "Tillar", "stat_free": "Bepul", "stat_est": "2026",
-    "footer_about": "Biz haqimizda", "footer_contact": "Aloqa", "footer_privacy": "Maxfiylik", "footer_terms": "Shartlar", "footer_disclaimer": "Ogohlantirish",
-    "search_empty_title": "Asboblar topilmadi", "search_empty_desc": "Boshqa so'z bilan qidiring yoki toifani o'zgartiring.",
+    /* --- NAVIGATION & GENERAL UI --- */
+    "nav_dashboard": "Boshqaruv paneli",
+    "nav_favorites": "Sevimlilar",
+    "nav_emergency": "SHOSHILINCH",
+    "search_placeholder": "Asboblarni qidirish (masalan, GCS, BMI)...",
+    "hero_pill": "TIBBIYOT TALABASI TOMONIDAN • MUTAXASSISLAR UCHUN YARATILGAN",
+    "hero_title_1": "Klinik asboblar",
+    "hero_title_2": "palata aylanmasi uchun",
+    "hero_subtitle": "4 tilda dalillarga asoslangan kalkulyatorlar va mnemonikalar. Tez, ishonchli va mobil qurilmalarga mos — barchasi bemor yotog'i yonida.",
+    "stat_tools": "Kalkulyatorlar",
+    "stat_langs": "Tillar",
+    "stat_free": "Bepul",
+    "stat_est": "2026-yilda asos solingan",
+    "search_empty_title": "Asboblar topilmadi",
+    "search_empty_desc": "Boshqa kalit so'zlar bilan urinib ko'ring yoki toifani o'zgartiring.",
 
-    "cat_all": "Barchasi", "cat_cardio": "Kardiologiya", "cat_pulmo": "Pulmonologiya", "cat_gastro": "Gastroenterologiya", "cat_nephro": "Nefrologiya", "cat_critical": "Reanimatsiya", "cat_surgery": "Xirurgiya", "cat_neuro": "Nevrologiya", "cat_general": "Umumiy va Pediatriya",
+    /* --- CATEGORIES --- */
+    "cat_all": "Barchasi",
+    "cat_cardio": "Kardiologiya",
+    "cat_pulmo": "Pulmonologiya",
+    "cat_gastro": "Gastroenterologiya",
+    "cat_nephro": "Nefrologiya",
+    "cat_critical": "Reanimatsiya",
+    "cat_surgery": "Xirurgiya",
+    "cat_neuro": "Nevrologiya",
+    "cat_general": "Umumiy va Pediatriya",
 
+    /* --- CLINICAL TOOLS (50) --- */
+    // Cardiology
     "tool_chads_title": "CHA2DS2-VASc shkalasi", "tool_chads_desc": "Atrial fibrilatsiyada insult xavfini baholash.",
     "tool_hasbled_title": "HAS-BLED shkalasi", "tool_hasbled_desc": "Atrial fibrilatsiyada yirik qon ketish xavfi.",
-    "tool_heart_title": "HEART shkalasi", "tool_heart_desc": "6 haftalik yurak xavfi prognozi.",
-    "tool_timi_title": "TIMI xavf shkalasi", "tool_timi_desc": "NSTEMI bemorlarida o'lim xavfi.",
+    "tool_heart_title": "HEART shkalasi", "tool_heart_desc": "6 haftalik yirik yurak xuruji xavfi prognozi.",
+    "tool_timi_title": "TIMI xavf shkalasi", "tool_timi_desc": "NSTEMI bemorlarida o'lim xavfi ko'rsatkichi.",
     "tool_grace_title": "GRACE shkalasi", "tool_grace_desc": "O'tkir koronar sindromda 6 oylik o'lim ehtimoli.",
-    "tool_ascvd_title": "ASCVD xavfi", "tool_ascvd_desc": "Yurak-qon tomir kasalliklarining 10 yillik xavfi.",
-    "tool_framingham_title": "Framingem shkalasi", "tool_framingham_desc": "Yurak kasalligi rivojlanishining 10 yillik xavfi.",
-    "tool_qtc_title": "QTc kalkulyatori", "tool_qtc_desc": "Bazett formulasi yordamida korreksiya qilingan QT.",
-
-    "tool_wells_title": "O'YE uchun Wells", "tool_wells_desc": "O'pka yallig'lanishining klinik ehtimoli.",
+    "tool_ascvd_title": "ASCVD xavf bahosi", "tool_ascvd_desc": "Yurak-qon tomir kasalliklarining 10 yillik xavfi.",
+    "tool_framingham_title": "Framingem shkalasi", "tool_framingham_desc": "Yurak ishemik kasalligi rivojlanishining 10 yillik xavfi.",
+    "tool_qtc_title": "QTc kalkulyatori", "tool_qtc_desc": "Bazett formulasi bo'yicha korreksiya qilingan QT intervali.",
+    
+    // Pulmonology
+    "tool_wells_title": "Wells mezonlari (O'YE)", "tool_wells_desc": "O'pka arteriyasi emboliyasining klinik ehtimoli.",
     "tool_perc_title": "PERC qoidasi", "tool_perc_desc": "Kam xavfli bemorlarda O'YE ni istisno qilish.",
-    "tool_curb_title": "CURB-65 shkalasi", "tool_curb_desc": "Pnevmoniya og'irligi va kasalxonaga yotqizish.",
-    "tool_psi_title": "PSI / PORT shkalasi", "tool_psi_desc": "O'limni prognoz qilish uchun pnevmoniya indeksi.",
-    "tool_lights_title": "Light mezonlari", "tool_lights_desc": "Ekssudat va transsudatni farqlash.",
-    "tool_bode_title": "BODE indeksi", "tool_bode_desc": "O'SOK (COPD) da yashash davomiyligini baholash.",
-
+    "tool_curb_title": "CURB-65 shkalasi", "tool_curb_desc": "Pnevmoniya og'irligi va gospitalizatsiya mezonlari.",
+    "tool_psi_title": "PSI / PORT shkalasi", "tool_psi_desc": "O'limni prognoz qilish uchun pnevmoniya og'irlik indeksi.",
+    "tool_lights_title": "Light mezonlari", "tool_lights_desc": "Ekssudat va transsudat plevra suyuqligini farqlash.",
+    "tool_bode_title": "BODE indeksi", "tool_bode_desc": "O'SOK (COPD) bilan og'rigan bemorlarda yashash davomiyligi.",
+    
+    // Gastroenterology
     "tool_child_title": "Child-Pugh shkalasi", "tool_child_desc": "Jigar sirrozida prognoz va o'lim xavfi.",
-    "tool_meld_title": "MELD shkalasi", "tool_meld_desc": "Jigar kasalligi terminal bosqichida o'lim xavfi.",
-    "tool_meldna_title": "MELD-Na shkalasi", "tool_meldna_desc": "Jigar transplantatsiyasi uchun natriy korreksiyasi.",
-    "tool_gbs_title": "Glazgo-Blatchford", "tool_gbs_desc": "Yuqori GI qon ketishida aralashuvlar skriningi.",
-    "tool_rockall_title": "Rockall shkalasi", "tool_rockall_desc": "Yuqori GI qon ketishidan keyin o'lim xavfi.",
-    "tool_maddrey_title": "Maddrey funksiyasi", "tool_maddrey_desc": "Alkogolli gepatitda steroidga ehtiyoj va prognoz.",
-
-    "tool_egfr_title": "eGFR kalkulyatori", "tool_egfr_desc": "CKD-EPI formulasi bo'yicha buyrak funksiyasi.",
-    "tool_crcl_title": "Kreatinin klirensi", "tool_crcl_desc": "Dori dozasi uchun Cockcroft-Gault formulasi.",
-    "tool_fena_title": "FENa", "tool_fena_desc": "O'tkir buyrak zararlanishida natriy ekskretsiyasi.",
-    "tool_feurea_title": "FEUrea", "tool_feurea_desc": "Diuretiklar fonida karbamid ekskretsiyasi.",
+    "tool_meld_title": "MELD shkalasi", "tool_meld_desc": "Jigar kasalligining terminal bosqichida 3 oylik o'lim xavfi.",
+    "tool_meldna_title": "MELD-Na shkalasi", "tool_meldna_desc": "Jigar transplantatsiyasi uchun natriy bilan sozlangan MELD.",
+    "tool_gbs_title": "Glazgo-Blatchford", "tool_gbs_desc": "Yuqori GI qon ketishida aralashuvlar zarurligi skriningi.",
+    "tool_rockall_title": "Rockall shkalasi", "tool_rockall_desc": "GI qon ketishidan keyingi o'lim xavfi.",
+    "tool_maddrey_title": "Maddrey funksiyasi", "tool_maddrey_desc": "Alkogolli gepatitda steroid davo zarurati va prognoz.",
+    
+    // Nephrology
+    "tool_egfr_title": "eGFR kalkulyatori", "tool_egfr_desc": "CKD-EPI formulasi bo'yicha buyrak funksiyasini baholash.",
+    "tool_crcl_title": "Kreatinin klirensi", "tool_crcl_desc": "Dori dozalari uchun Cockcroft-Gault formulasi.",
+    "tool_fena_title": "FENa", "tool_fena_desc": "O'tkir buyrak zararlanishi etiologiyasi (natriy ekskretsiyasi).",
+    "tool_feurea_title": "FEUrea", "tool_feurea_desc": "Diuretiklar fonida karbamidning fraksion ekskretsiyasi.",
     "tool_aniongap_title": "Anion farqi", "tool_aniongap_desc": "Metabolik atsidozni toifalash.",
-    "tool_winters_title": "Winters formulasi", "tool_winters_desc": "Metabolik atsidozda respirator kompensatsiya.",
-    "tool_calcium_title": "Korreksiya qilingan kalsiy", "tool_calcium_desc": "Gipoalbuminemiyada haqiqiy kalsiy darajasi.",
-    "tool_sodium_title": "Korreksiya qilingan natriy", "tool_sodium_desc": "Giperglikemiyada haqiqiy natriy darajasi.",
-
-    "tool_gcs_title": "Glazgo koma shkalasi", "tool_gcs_desc": "Ong darajasini baholash uchun nevrologik matritsa.",
-    "tool_sofa_title": "SOFA shkalasi", "tool_sofa_desc": "Sepsis o'limi va a'zolar yetishmovchiligi.",
-    "tool_qsofa_title": "qSOFA shkalasi", "tool_qsofa_desc": "Sepsis mezonlari uchun tezkor skrining.",
-    "tool_apache_title": "APACHE II shkalasi", "tool_apache_desc": "Reanimatsiyada kasallik og'irligi klassifikatsiyasi.",
-    "tool_parkland_title": "Parkland formulasi", "tool_parkland_desc": "Kuyishlarda 24 soatlik suyuqlik reanimatsiyasi.",
-    "tool_ivfluids_title": "Qo'llab-quvvatlovchi infuziya", "tool_ivfluids_desc": "Vena ichiga suyuqlik yuborish uchun 4-2-1 qoidasi.",
-    "tool_nexus_title": "NEXUS mezonlari", "tool_nexus_desc": "Bo'yin umurtqasini vizualizatsiyasiz istisno qilish.",
-    "tool_canadian_title": "Kanada KT qoidasi", "tool_canadian_desc": "Yengil bosh miya jarohatlarida KT ko'rsatmalari.",
-
-    "tool_alvarado_title": "Alvarado shkalasi", "tool_alvarado_desc": "O'tkir appenditsitning klinik ehtimoli.",
-    "tool_ranson_title": "Ranson mezonlari", "tool_ranson_desc": "O'tkir pankreatitda o'lim prognozi.",
-    "tool_caprini_title": "Caprini shkalasi", "tool_caprini_desc": "Jarrohlik bemorlarida DVT xavfini baholash.",
-    "tool_nsqip_title": "NSQIP kalkulyatori", "tool_nsqip_desc": "Operatsiyadan keyingi asoratlarni prognoz qiladi.",
-
-    "tool_nihss_title": "NIHSS insult shkalasi", "tool_nihss_desc": "O'tkir miya infarktining og'irligini belgilaydi.",
-    "tool_abcd2_title": "ABCD2 shkalasi", "tool_abcd2_desc": "TIA dan keyin insult xavfini prognoz qilish.",
-    "tool_hunt_title": "Hunt va Hess shkalasi", "tool_hunt_desc": "Subaraknoidal qon ketishida o'lim prognozi.",
-    "tool_chalice_title": "CHALICE qoidasi", "tool_chalice_desc": "Bolalar BMT da KT ga ehtiyojni aniqlaydi.",
-
-    "tool_apgar_title": "APGAR shkalasi", "tool_apgar_desc": "Tug'ilgandan keyin chaqaloq holatini tezkor baholash.",
-    "tool_holliday_title": "Holliday-Segar formulasi", "tool_holliday_desc": "Bolalar uchun suyuqlik va kaloriya ehtiyoji.",
+    "tool_winters_title": "Winters formulasi", "tool_winters_desc": "Metabolik atsidozda kutilayotgan respirator kompensatsiya.",
+    "tool_calcium_title": "Kalsiy korreksiyasi", "tool_calcium_desc": "Gipoalbuminemiyada haqiqiy kalsiy darajasi.",
+    "tool_sodium_title": "Natriy korreksiyasi", "tool_sodium_desc": "Giperglikemiyada haqiqiy natriy darajasi.",
+    
+    // Critical Care
+    "tool_gcs_title": "Glazgo koma shkalasi (GCS)", "tool_gcs_desc": "Ong darajasini baholash uchun nevrologik matritsa.",
+    "tool_sofa_title": "SOFA shkalasi", "tool_sofa_desc": "Sepsisda a'zolar yetishmovchiligi va o'lim xavfi.",
+    "tool_qsofa_title": "qSOFA shkalasi", "tool_qsofa_desc": "Sepsis mezonlari uchun tezkor prikrovat skriningi.",
+    "tool_apache_title": "APACHE II shkalasi", "tool_apache_desc": "Reanimatsiyada kasallik og'irligi va o'lim prognozi.",
+    "tool_parkland_title": "Parkland formulasi", "tool_parkland_desc": "Kuyishlarda 24 soatlik suyuqlik reanimatsiyasi hajmi.",
+    "tool_ivfluids_title": "Suyuqlik infuziyasi", "tool_ivfluids_desc": "Vena ichiga suyuqlik yuborish uchun 4-2-1 qoidasi.",
+    "tool_nexus_title": "NEXUS mezonlari", "tool_nexus_desc": "Bo'yin umurtqasini KT siz klinik istisno qilish.",
+    "tool_canadian_title": "Kanada KT qoidasi", "tool_canadian_desc": "Yengil bosh miya jarohatlarida KT ga ko'rsatmalar.",
+    
+    // Surgery
+    "tool_alvarado_title": "Alvarado shkalasi", "tool_alvarado_desc": "O'tkir appenditsit rivojlanishining klinik ehtimoli.",
+    "tool_ranson_title": "Ranson mezonlari", "tool_ranson_desc": "O'tkir pankreatitda o'lim xavfi prognozi.",
+    "tool_caprini_title": "Caprini shkalasi", "tool_caprini_desc": "Jarrohlik bemorlarida VTE va DVT xavfini baholash.",
+    "tool_nsqip_title": "NSQIP kalkulyatori", "tool_nsqip_desc": "Operatsiyadan keyingi asoratlarni prognoz qilish.",
+    
+    // Neurology
+    "tool_nihss_title": "NIHSS insult shkalasi", "tool_nihss_desc": "O'tkir miya infarktining og'irlik darajasini o'lchash.",
+    "tool_abcd2_title": "ABCD2 shkalasi", "tool_abcd2_desc": "TIA dan keyingi kunlarda insult xavfi prognozi.",
+    "tool_hunt_title": "Hunt va Hess shkalasi", "tool_hunt_desc": "Subaraknoidal qon ketishida o'lim ehtimoli.",
+    "tool_chalice_title": "CHALICE qoidasi", "tool_chalice_desc": "Bolalar bosh miya jarohatida KT zarurligini aniqlash.",
+    
+    // General & Peds
+    "tool_apgar_title": "APGAR shkalasi", "tool_apgar_desc": "Yangi tug'ilgan chaqaloq holatini baholash (1 va 5 daqiqa).",
+    "tool_holliday_title": "Holliday-Segar formulasi", "tool_holliday_desc": "Bolalar uchun sutkalik suyuqlik va kaloriya ehtiyoji.",
     "tool_bishop_title": "Bishop shkalasi", "tool_bishop_desc": "Tug'ruqni qo'zg'atish uchun bachadon bo'yni tayyorligi.",
-    "tool_centor_title": "Centor shkalasi", "tool_centor_desc": "A guruhi Streptokokk faringiti ehtimoli.",
-    "tool_bmi_title": "TMI va TYY", "tool_bmi_desc": "Dori dozasi uchun tana massasi va yuzasi.",
-    "tool_ecog_title": "ECOG holati", "tool_ecog_desc": "Onkologik bemorlar uchun funksional baholash."
-};
+    "tool_centor_title": "Centor shkalasi", "tool_centor_desc": "A guruhi streptokokkli faringit ehtimoli.",
+    "tool_bmi_title": "TMI va TYY", "tool_bmi_desc": "Tana massasi indeksi va yuzasi (dozalash uchun).",
+    "tool_ecog_title": "ECOG holati", "tool_ecog_desc": "Onkologik bemorlarning funksional holatini baholash.",
+
+    /* --- FOOTER LINKS --- */
+    "footer_about": "Biz haqimizda",
+    "footer_contact": "Aloqa",
+    "footer_privacy": "Maxfiylik",
+    "footer_terms": "Shartlar",
+    "footer_disclaimer": "Rad etish",
+
+    /* --- EXTENDED CLINICAL MANIFESTO (ABOUT US) --- */
+    "about_manifesto": "Klinik manifest",
+    "about_title": "WardCalc haqida: Bedside diagnostikasining kelajagi",
+    "about_highlight": "Nazariya va amaliyot.",
+    "about_intro": "WardCalc klinik voqelikdan tug'ilgan: inson miyasi mantiq uchun mukammal, ammo formulalarni yodlash uchun zaifdir.",
+    "about_p1": "WardCalc yordam ko'rsatish joyida ma'lumotlarni qayta ishlashda tub burilishni anglatadi. Buxoro davlat tibbiyot instituti (BSMI) bitiruvchi talabasi tomonidan yaratilgan ushbu platforma shifokorlarning kognitiv yukini kamaytirishga xizmat qiladi. Tibbiy bilimlar har necha oyda ikki baravar ko'payib borayotgan davrda, WardCalc shifokorga formulalarni yodlashga emas, balki bemorga e'tibor qaratishga imkon beradi.",
+    "about_p2": "Arxitektura uchta ustunga asoslangan: Tezlik, Aniqlik va Foydalanish qulayligi. Boshqa ilovalar ro'yxatdan o'tish va reklamalar bilan vaqtni olsa, WardCalc shoshilinch holatlarda va palata aylanmasida bir necha soniya ichida natija berish uchun optimallashtirilgan.",
+    "about_p3": "WardCalc jahon tibbiyot hamjamiyatiga xizmat qiladi. Bizning ko'p tilli tizimimiz (O'zbek, Ingliz, Nemis, Rus) xalqaro tizimlarda ishlayotgan yoki Fachsprachprüfung (FSP) kabi imtihonlarga tayyorlanayotgan shifokorlar uchun mo'ljallangan. Klinik asboblar iqtisodiy to'siqlarsiz, barcha uchun bepul bo'lishi kerak deb hisoblaymiz.",
+
+    /* --- CONTACT & COLLABORATION --- */
+    "contact_pill": "Global aloqa markazi",
+    "contact_title": "WardCalc tarmog'iga",
+    "contact_highlight": "qo'shiling.",
+    "contact_intro": "Tibbiyot — hamkorlik fanidir. Biz klinik fikr-mulohazalar, hamkorlik takliflari va akademik aloqalar uchun doimo ochiqmiz.",
+    "contact_h1": "Klinik qayta aloqa va Tekshiruv",
+    "contact_p1": "Zamonaviy tibbiyotda dasturiy ta'minot doimiy auditga muhtoj. WardCalc ning har bir algoritmi fiziologiya bilan ishlash uchun yaratilgan. Biz foydalanuvchilarimizni faol tekshiruvchilar deb bilamiz. Agar xatolik topsangiz, biz uchun bu eng ustuvor vazifadir.",
+    "contact_h2": "Akademik integratsiya",
+    "contact_p3": "Biz universitetlar va Prime Diagnostics kabi klinikalar bilan WardCalc ni o'quv dasturlariga tatbiq etish bo'yicha hamkorlikni qidirmoqdamiz. 2D-GenMed 21 kabi guruhlar uchun maxsus yechimlarni qo'llab-quvvatlaymiz.",
+    "contact_form_title": "To'g'ridan-to'g'ri so'rov protokoli",
+    "contact_btn_send": "Xabarni yuborish",
+    "contact_label_name": "To'liq ismingiz va unvoningiz",
+    "contact_label_email": "Institutsional elektron pochta",
+    "contact_label_subject": "So'rov turi",
+    "contact_opt_1": "Klinik fikr-mulohaza",
+    "contact_opt_2": "Institutsional hamkorlik",
+    "contact_opt_3": "FSP va til bo'yicha hamkorlik",
+    "contact_opt_4": "Media va hujjatli loyihalar",
+    "contact_opt_5": "Umumiy savollar",
+    "contact_label_message": "Batafsil taklif",
+
+    /* --- MEDICAL DISCLAIMER --- */
+    "disc_pill": "Klinik ogohlantirish",
+    "disc_title": "To'liq",
+    "disc_highlight": "Tibbiy rad etish bildiruvnomasi.",
+    "disc_intro": "2026-yil may oyidan kuchga kirgan. Ushbu hujjat algoritmlar va shifokor mas'uliyati o'rtasidagi huquqiy chegarani belgilaydi.",
+    "disc_h1": "1. Asosiy maqsad va diagnostika emasligi",
+    "disc_p1_1": "WardCalc ma'lumot beruvchi asbobdir va u sertifikatlangan diagnostik tibbiy qurilma EMAS. U FDA yoki EMA tomonidan mustaqil qaror qabul qilish uchun baholanmagan.",
+    "disc_p1_2": "Ma'lumotlar faqat ta'lim maqsadida beriladi. Shifokorning mustaqil klinik xulosasi platformadan foydalanishning majburiy shartidir.",
+    "disc_h2": "2. Shifokor mas'uliyati",
+    "disc_p2_1": "Hech qanday algoritm inson fiziologiyasini to'liq qamrab ololmaydi. Foydalanuvchi har qanday klinik aralashuv uchun 100% javobgardir. Shifoxona protokollariga zid ravishda natijalarga ko'r-ko'rona ergashish mumkin emas.",
+
+    /* --- PRIVACY POLICY --- */
+    "priv_pill": "Huquq va Komplaens",
+    "priv_title": "Klinik ma'lumotlar va",
+    "priv_highlight": "Maxfiylik siyosati.",
+    "priv
